b7308e74c396d1f2c46f8d51941732401d59bbad
foxcornio <foxygamercompany@gmail.com>
2025-07-30 00:34:19 -0600

    Se empieza a practicar el pep8 sin scripts de ayuda para saber como emplear estas practicas

--- practice/semana6/verify.py
+++ practice/semana6/verify.py
@@ -21,23 +21,23 @@ text_input = "" # this variable is type string
 while sesson == False and count < 2: # this while execute the main instruccion
     text_input = input("Ingresa la contraseña:\n") # Request to user into a password
     res = verifyNumber(text_input) # res storage the answer to call to function verifyNumber and pass how param the value in text_input.
-    while res == False and count < 2:
-        count += 1
-        text_input = input("La contraseña debe tener almenos 1 numero.\nIngresa una contraseña:\n")
-        res = verifyNumber(text_input)
-    if res == True:
+    while res == False and count < 2: # this while be execute if the variable res is false and it counter is minus to two
+        count += 1 # add one to counter
+        text_input = input("La contraseña debe tener almenos 1 numero.\nIngresa una contraseña:\n") # remmember to user that is necesary one number in the password
+        res = verifyNumber(text_input) # recall to function verifyNumber
+    if res == True: # if variable res is true then program request into the password again for verify that this is equals
         password = text_input
         text_input = input("Ingresa nuevamente la contraseña:\n")
-        res = verifyPassword(password, text_input)
-    while res == False and count < 2:
-        count += 1
-        text_input = input("Las contraseñas no coinciden.\nPorfavor ingresa la misma contraseña:\n")
-        res = verifyPassword(password, text_input)
-    if res == True:
+        res = verifyPassword(password, text_input) # call to the function verifyPassword and storage the answer in res
+    while res == False and count < 2: # while res is equal to false and counter is minus to two, execute the next code
+        count += 1 # add one to counter
+        text_input = input("Las contraseñas no coinciden.\nPorfavor ingresa la misma contraseña:\n") # notify to user that your password is diferente
+        res = verifyPassword(password, text_input) # call to function verifyPassword and storage the answer in res
+    if res == True: # if res is true then sesson change your value to true
         sesson = True
 
-if sesson == True:
+if sesson == True: # if sesson is true, notify to user that all right, and your password succesfull
     print("Contraseñas coinciden con éxito.")
-elif count > 1:
+elif count > 1: # if count is major to one, notifye to user that him had three wrong
     print("Fallaste en 3 intentos, por seguridad intentelo de nuevo más tarde.")
 sys.exit()
